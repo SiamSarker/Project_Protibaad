d96956bbc45f25417a927ffeb38ea72430cc1d35
swapnil072 <sbiswas192072@bscse.uiu.ac.bd>
2022-05-22 13:47:21 +0600

    function added

--- missing_create.php
+++ missing_create.php
@@ -22,7 +22,7 @@ if (isset($_POST['submit'])) {
                 }
             }
 
-            // Check if file already exists
+        
             if (file_exists($target_file)) {
                 $uploadOk = 0;
             }
