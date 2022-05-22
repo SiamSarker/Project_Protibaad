90c4795965d423eaa151b300c25a67e910f4926a
Shakil2424 <78400583+Shakil2424@users.noreply.github.com>
2022-05-22 14:07:40 +0600

    Update blog_create.php
    new feature added

--- blog_create.php
+++ blog_create.php
@@ -36,7 +36,6 @@ if (isset($_POST['submit'])) {
                 // if everything is ok, try to upload file
             } else {
                 if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-                    //  echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
                 } else {
                     echo "<script>alert('Sorry, there was an error uploading your file.')</script>";
                 }
