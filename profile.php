7e0137e4277d823808274ce6d58de7fda277c96c
promaprogga <86119860+promaprogga@users.noreply.github.com>
2022-05-22 11:52:50 +0600

    Add files via upload

--- profile.php
+++ profile.php
@@ -1,23 +1,48 @@
 <?php
+include 'includes/connection.php';
+?>
+<?php
 session_start();
-include 'config.php';
+function str_openssl_dec($str, $iv){
+    $key='progga1234567890#%$%$#$%$';
+    $chiper="AES-128-CTR";
+    $options=0;
+    $str=openssl_decrypt($str, $chiper, $key, $options, $iv);
+    return $str;
+ 
+ }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-
-    <meta charset="utf-8">
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Protibaaad</title>
+    <link rel="icon" type="image" href="image/icon.png">
+
+    <!-- <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" /> -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> 
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/welcome.css">
 
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="write_review.css">
 
-    <meta charset="utf-8">
 </head>
 
 <body>
+<?php include('includes/user_navbar.php') ?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
     <div class="container">
         <div class="row">
             <div class="col-md-8 offset-md-2 mt-5">
@@ -29,15 +54,19 @@ include 'config.php';
                         </tr>
                     </thead>
                     <?php
+
                     $username =  $_SESSION['username'];
-                    $sql = "SELECT username, email FROM  users WHERE `username`='$username'";
+                    $sql = "SELECT * FROM  users WHERE `username`='$username'";
                     $result = $conn->query($sql);
 
                     if ($result->num_rows > 0) {
 
                         while ($row = $result->fetch_assoc()) {
+                            $iv=$row['iv'];
+                            $iv=hex2bin($iv);
                             $username = $row["username"];
                             $email = $row["email"];
+                            $email =str_openssl_dec($email, $iv);
                     ?>
                             <tbody>
                                 <tr>
@@ -50,7 +79,17 @@ include 'config.php';
                                     <th scope="row">Email : </th>
                                     <td><?php echo $email; ?></td>
                                 </tr>
+                                <tr>
+                                    <td>
+
+                                    </td>
+                                    <td>
+                                        
+                                        <a href="change-password.php">Update Password</a>
+                                    </td>
+                                </tr>
                             </tbody>
+
                     <?php
                         }
                     }
@@ -61,6 +100,18 @@ include 'config.php';
 
         </div>
     </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <?php include('includes/user_footer.php') ?>
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+    <script src="js/script.js"></script>
 
 </body>
 
